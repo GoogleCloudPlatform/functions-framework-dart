93adb84dc16979ee19eb92031409ef024892e386
Kevin Moore <kevmoo@google.com>
2020-11-24 11:28:06 -0800

    Annotate CloudFunction so it's clear it should only apply to Functions

--- functions_framework/lib/functions_framework.dart
+++ functions_framework/lib/functions_framework.dart
@@ -2,6 +2,9 @@
 // Please see the AUTHORS file or details. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'package:meta/meta_meta.dart';
+
+@Target({TargetKind.function})
 class CloudFunction {
   final String target;
 
