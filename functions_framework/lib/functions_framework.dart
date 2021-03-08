ae5562be12f69ebeebc7e91fe923a5cc5b499180
Jorge Carlos <skay_1994@yahoo.com.br>
2021-03-08 13:06:26 -0300

    Export FunctionConfig

--- functions_framework/lib/functions_framework.dart
+++ functions_framework/lib/functions_framework.dart
@@ -15,6 +15,7 @@
 export 'src/bad_request_exception.dart' show BadRequestException;
 export 'src/cloud_event.dart' show CloudEvent;
 export 'src/cloud_function.dart' show CloudFunction;
+export 'src/function_config.dart' show FunctionConfig;
 export 'src/log_severity.dart' show RequestLogger, LogSeverity;
 export 'src/request_context.dart' show RequestContext;
 export 'src/typedefs.dart'
