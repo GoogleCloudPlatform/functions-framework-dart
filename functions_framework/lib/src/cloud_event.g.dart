28bc425f37e4fac70497cff219c3d680cf82bb83
Kevin Moore <kevmoo@google.com>
2021-07-15 11:49:46 -0700

    builder: support the latest pkg:analyzer, prepare for release
    Bump lock files across the board
    Update generated code

--- functions_framework/lib/src/cloud_event.g.dart
+++ functions_framework/lib/src/cloud_event.g.dart
@@ -8,31 +8,36 @@ part of 'cloud_event.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-CloudEvent _$CloudEventFromJson(Map<String, dynamic> json) {
-  return $checkedNew('CloudEvent', json, () {
-    $checkKeys(json,
-        requiredKeys: const ['id', 'source', 'specversion', 'type']);
-    final val = CloudEvent(
-      id: $checkedConvert(json, 'id', (v) => v as String),
-      source: $checkedConvert(json, 'source', (v) => Uri.parse(v as String)),
-      specVersion: $checkedConvert(json, 'specversion', (v) => v as String),
-      type: $checkedConvert(json, 'type', (v) => v as String),
-      data: $checkedConvert(json, 'data', (v) => v),
-      dataContentType:
-          $checkedConvert(json, 'datacontenttype', (v) => v as String?),
-      dataSchema: $checkedConvert(
-          json, 'dataschema', (v) => v == null ? null : Uri.parse(v as String)),
-      subject: $checkedConvert(json, 'subject', (v) => v as String?),
-      time: $checkedConvert(
-          json, 'time', (v) => v == null ? null : DateTime.parse(v as String)),
+CloudEvent _$CloudEventFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'CloudEvent',
+      json,
+      ($checkedConvert) {
+        $checkKeys(
+          json,
+          requiredKeys: const ['id', 'source', 'specversion', 'type'],
+        );
+        final val = CloudEvent(
+          id: $checkedConvert('id', (v) => v as String),
+          source: $checkedConvert('source', (v) => Uri.parse(v as String)),
+          specVersion: $checkedConvert('specversion', (v) => v as String),
+          type: $checkedConvert('type', (v) => v as String),
+          data: $checkedConvert('data', (v) => v),
+          dataContentType:
+              $checkedConvert('datacontenttype', (v) => v as String?),
+          dataSchema: $checkedConvert(
+              'dataschema', (v) => v == null ? null : Uri.parse(v as String)),
+          subject: $checkedConvert('subject', (v) => v as String?),
+          time: $checkedConvert(
+              'time', (v) => v == null ? null : DateTime.parse(v as String)),
+        );
+        return val;
+      },
+      fieldKeyMap: const {
+        'specVersion': 'specversion',
+        'dataContentType': 'datacontenttype',
+        'dataSchema': 'dataschema'
+      },
     );
-    return val;
-  }, fieldKeyMap: const {
-    'specVersion': 'specversion',
-    'dataContentType': 'datacontenttype',
-    'dataSchema': 'dataschema'
-  });
-}
 
 Map<String, dynamic> _$CloudEventToJson(CloudEvent instance) {
   final val = <String, dynamic>{
