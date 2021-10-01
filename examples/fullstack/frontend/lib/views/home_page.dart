f6916432128b22bf0b9a03143724303bd0b5e813
Kevin Moore <kevmoo@google.com>
2021-10-01 12:58:27 -0700

    refactor: Remove unneeded import

--- examples/fullstack/frontend/lib/views/home_page.dart
+++ examples/fullstack/frontend/lib/views/home_page.dart
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'greeting.dart';
