b4732ea6e1fa9301ef43a148ed2ee2c3b581dc88
Kevin Moore <kevmoo@google.com>
2023-06-15 15:00:14 -0700

    regenerate build output

--- examples/json/bin/server.dart
+++ examples/json/bin/server.dart
@@ -20,32 +20,28 @@ Future<void> main(List<String> args) async {
   await serve(args, _nameToFunctionTarget);
 }
 
-FunctionTarget? _nameToFunctionTarget(String name) {
-  switch (name) {
-    case 'function':
-      return JsonFunctionTarget(
-        function_library.function,
-        (json) {
-          if (json is Map<String, dynamic>) {
-            try {
-              return function_library.GreetingRequest.fromJson(json);
-            } catch (e, stack) {
-              throw BadRequestException(
-                400,
-                'There was an error parsing the provided JSON data.',
-                innerError: e,
-                innerStack: stack,
-              );
+FunctionTarget? _nameToFunctionTarget(String name) => switch (name) {
+      'function' => JsonFunctionTarget(
+          function_library.function,
+          (json) {
+            if (json is Map<String, dynamic>) {
+              try {
+                return function_library.GreetingRequest.fromJson(json);
+              } catch (e, stack) {
+                throw BadRequestException(
+                  400,
+                  'There was an error parsing the provided JSON data.',
+                  innerError: e,
+                  innerStack: stack,
+                );
+              }
             }
-          }
-          throw BadRequestException(
-            400,
-            'The provided JSON is not the expected type '
-            '`Map<String, dynamic>`.',
-          );
-        },
-      );
-    default:
-      return null;
-  }
-}
+            throw BadRequestException(
+              400,
+              'The provided JSON is not the expected type '
+              '`Map<String, dynamic>`.',
+            );
+          },
+        ),
+      _ => null
+    };
