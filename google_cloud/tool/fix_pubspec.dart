55a6feb7f4fd430279e841e4ae05d3858636645a
Kevin Moore <kevmoo@google.com>
2024-08-19 16:36:47 -0700

    nits

--- google_cloud/tool/fix_pubspec.dart
+++ google_cloud/tool/fix_pubspec.dart
@@ -2,6 +2,9 @@
 
 import 'dart:io';
 
+// Removes the `workspace` property from the pubspec so it can be used via
+// Docker.
+// Work-around for https://github.com/dart-lang/pub/issues/4357
 void main(List<String> args) {
   final file = File('pubspec.yaml');
   final content = file.readAsStringSync().replaceAll(_content, '');
