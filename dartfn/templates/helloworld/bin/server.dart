f8624137026269b1e2925d6c02b6bf7c85953e38
Kevin Moore <kevmoo@users.noreply.github.com>
2026-02-10 22:36:25 -0800

    fix: move to latest pkg:melos (#512)

--- dartfn/templates/helloworld/bin/server.dart
+++ dartfn/templates/helloworld/bin/server.dart
@@ -5,13 +5,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// @dart=3.6
 
 import 'package:__helloworld__/functions.dart' as function_library;
 import 'package:functions_framework/serve.dart';
@@ -21,6 +23,8 @@ Future<void> main(List<String> args) async {
 }
 
 FunctionTarget? _nameToFunctionTarget(String name) => switch (name) {
-  'function' => FunctionTarget.http(function_library.function),
-  _ => null,
-};
+      'function' => FunctionTarget.http(
+          function_library.function,
+        ),
+      _ => null
+    };
